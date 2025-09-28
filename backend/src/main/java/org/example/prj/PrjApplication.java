9c84fffbb185f28a3b19d46ac60e804c39694bdb
BaggioHin <tronghien03@gmail.com>
2025-09-28 13:03:00 +0700

    feat:Add entity package

---
+++ backend/src/main/java/org/example/prj/PrjApplication.java
@@ -0,0 +1,13 @@
+package org.example.OnlineLibMana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplicationOnline
+public class PrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjApplication.class, args);
+    }
+
+}
