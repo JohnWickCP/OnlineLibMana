7986d9dd7fbce8f2f2606b1c98e7c86856d1d389
BaggioHin <tronghien03@gmail.com>
2025-10-16 22:56:10 +0700

    Feat:add Api statistical for Admin

---
+++ backend/src/main/java/org/example/prj/controller/AdminController.java
@@ -0,0 +1,24 @@
+package org.example.prj.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.prj.DTO.Response.ApiResponse;
+import org.example.prj.entity.Dashboard;
+import org.example.prj.service.AdminService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    private AdminService adminService;
+
+    @GetMapping("/dashboard")
+    public ApiResponse<Dashboard> getDashboard() {
+        return ApiResponse.<Dashboard>builder()
+                .result(adminService.getDashboard())
+                .build();
+    }
+}
