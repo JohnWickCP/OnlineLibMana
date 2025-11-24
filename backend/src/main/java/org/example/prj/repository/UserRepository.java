ca947b2e34843998aae8f353853e13a36c93522b
BaggioHin <146904567+BaggioHin@users.noreply.github.com>
2025-11-24 23:10:35 +0700

    Update import

--- backend/src/main/java/org/example/prj/repository/UserRepository.java
+++ backend/src/main/java/org/example/prj/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package org.example.prj.repository;
 
+import jakarta.transaction.Transactional;
 import org.example.prj.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
